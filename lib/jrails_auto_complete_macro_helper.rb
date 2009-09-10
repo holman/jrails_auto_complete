d8a7d01f015828df66626a121067cd481af646ab
holman <github.com@zachholman.com>
2009-09-10 15:24:42 -0700

    Question the model directly in order for non-database virtual attributes to be queried.

--- lib/jrails_auto_complete_macro_helper.rb
+++ lib/jrails_auto_complete_macro_helper.rb
@@ -35,7 +35,7 @@ module JrailsAutoCompleteMacroHelper
 
   def auto_complete_result(entries, field, phrase = nil)
     return unless entries
-    items = entries.map { |entry| content_tag('li', phrase ? highlight(entry[field], phrase) : h(entry[field])) }
+    items = entries.map { |entry| content_tag('li', phrase ? highlight(entry.send(field), phrase) : h(entry.send(field))) }
     content_tag('ul', items.uniq)
   end
 
